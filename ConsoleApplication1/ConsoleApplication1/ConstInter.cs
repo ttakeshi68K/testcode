7c08e0e773e9b972ddd8a611d460c1940df3cd8a
takeno <takeno@cc-arc.co.jp>
2017-11-24 16:22:57 +0900

    とりあえずログが出るもの

---
+++ ConsoleApplication1/ConsoleApplication1/ConstInter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class C_LOG_NAME
+    {
+        public const string CALL_LOG = "CallLog";
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using log4net;
+using log4net.Config;
+
+using ConsoleApplication1;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        /// <summary>
+        /// ログ出力機能
+        /// </summary>
+        private static ILog _cLog = LogManager.GetLogger(C_LOG_NAME.CALL_LOG);
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool l4nSetingLoadSucess = _cLog.Logger.Repository.Configured;
+
+                if (!l4nSetingLoadSucess)
+                {
+                    var errorIventMsg = "log4net.xml load Error:Repository.Configured=" + l4nSetingLoadSucess.ToString();
+
+                    EventLog.WriteEntry("TESTPG", errorIventMsg, EventLogEntryType.Error, 9999);
+
+                    throw new Exception();
+                }
+
+                _cLog.Debug("TEST MSG Debug");
+
+                _cLog.Info("TEST MSG Info");
+
+            }
+            catch {
+
+            }
+        }
+    }
+}
