8e251c23b4f956ac5dfd8e97fbdb1d7bc6ebca9a
takeno <takeno@cc-arc.co.jp>
2017-11-27 13:59:27 +0900

    Entity FranmWork追加

---
+++ ConsoleApplication1/ConsoleApplication1/PGTXmlStrreamer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ConsoleApplication1
+{
+    class PGTXmlStrreamer
+
+    {
+        // 指定された型で取り込む
+        public static object Import(string path, Type type)
+        {
+            XmlSerializer seri = new XmlSerializer(type);
+
+            FileStream fs = null;
+            object dat = null;
+            try
+            {
+                fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                dat = seri.Deserialize(fs);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+
+            return dat;
+        }
+
+        // オブジェクトの出力
+        public static void Export(string path, object dat)
+        {
+            FileStream fs = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+
+                XmlSerializer seri = new XmlSerializer(dat.GetType());
+
+                seri.Serialize(fs, dat);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+        }
+
+
+    }
+}
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Configuration;
 
+// SQLServer
+using System.Data.SqlClient;
+
+// log4net
 using log4net;
-using log4net.Config;
 
-using ConsoleApplication1;
+using ConsoleApplication1.Properties;
 
 namespace ConsoleApplication1
 {
@@ -21,8 +21,6 @@ namespace ConsoleApplication1
 
         static void Main(string[] args)
         {
-            try
-            {
                 bool l4nSetingLoadSucess = _cLog.Logger.Repository.Configured;
 
                 if (!l4nSetingLoadSucess)
@@ -34,14 +32,61 @@ namespace ConsoleApplication1
                     throw new Exception();
                 }
 
-                _cLog.Debug("TEST MSG Debug");
+            _cLog.Info("*** TEST AP START ***");
 
-                _cLog.Info("TEST MSG Info");
+            // _cLog.Debug("TEST MSG Debug");
+            // _cLog.Info("TEST MSG Info");
+
+
+            try
+            {
+
+                SettingReader.Initialize();
+
+                // 接続文字列を生成する
+                string connectionString = ConfigurationManager.ConnectionStrings["SQLCON"].ConnectionString;
+
+
+                // SqlConnection の新しいインスタンスを生成する (接続文字列を指定)
+                SqlConnection cSqlConnection = (
+                    new System.Data.SqlClient.SqlConnection(connectionString)
+                );
+
+                // データベース接続を開く
+                cSqlConnection.Open();
+
+
+                string sql1 = "";
+
+
+
+
+
+                // データベース接続を閉じる (正しくは オブジェクトの破棄を保証する を参照)
+                cSqlConnection.Close();
+                cSqlConnection.Dispose();
+
+
 
             }
-            catch {
-
+            catch (Exception ex){
+                _cLog.Error("error",ex);
             }
+            finally
+            {
+                _cLog.Info("*** TEST AP END ***");
+            }
+        }
+    }
+    public class SettingReader {
+        internal static void Initialize()
+        {
+            string setingFilePath = PGTSetting.Default.SettingFilePath;
+            string setingFileName = PGTSetting.Default.SettingFileName;
+
+            // PGTXmlStrreamer.Import()
+
+
         }
     }
 }
